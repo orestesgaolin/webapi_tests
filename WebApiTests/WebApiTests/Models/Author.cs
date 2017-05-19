56216f101c1994aaa03d9621e5850555e5fa5455
Dominik Roszkowski <Dominik.Roszkowski@pentacomp.pl>
2017-05-19 11:45:22 +0200

    Add Author and Book models

---
+++ WebApiTests/WebApiTests/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTests.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ WebApiTests/WebApiTests/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTests.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
